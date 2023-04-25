6fa1789a0acd2974eb38bb6d182f1214933cb9f5
letheos <nintendoplayeraddict@gmail.com>
2023-04-25 13:15:17 +0200

    Ajout de la fonction vider, sauvegarde et chargerLabyrinthe

--- src/Element.java
+++ src/Element.java
@@ -1,4 +1,6 @@
-public class Element {
+import java.io.Serializable;
+
+public class Element implements Serializable {
     protected int bonus;
 
     public Element(int bonus){
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.io.IOException;
+
 public class Main {
 
     //x correspond a la hauteur
     //y correspond a la droite et la gauche
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Labyrinthe test;
         test = new Labyrinthe(5,5);
         System.out.println(test.GetCellules());
         System.out.println(test.GetCellule(2,2));
         test.CasserMur(2,2,"O");
+        test.GetCellule(0,0).murs.put("E",true);
         System.out.println(test.toString());
 
+        test.sauvegarderLabyrinthe("labyrinthetest.dat");
+        Labyrinthe récup = Labyrinthe.chargerLabyrinthe("labyrinthetest.dat");
+        System.out.println(récup.toString());
     }
 
 }
