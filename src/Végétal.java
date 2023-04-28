cde94a835ca8b16f2da1e1f3086785d665ac0c24
letheos <nintendoplayeraddict@gmail.com>
2023-04-28 15:07:19 +0200

    ajout des fonction CasserMur et Poser

---
+++ src/Végétal.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Végétal extends Element implements Serializable {
+    protected int bonus;
+
+    public Végétal(int bonus){
+        this.bonus = bonus;
+    }
+}
