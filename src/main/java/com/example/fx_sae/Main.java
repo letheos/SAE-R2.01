c7866f8747d08d3954590bcecfcc8ff1c9dddb62
letheos <nintendoplayeraddict@gmail.com>
2023-05-04 16:17:01 +0200

    Mise a jour -> Lancez maintenant HelloApplication pour travailler sur le main

--- src/main/java/com/example/fx_sae/Main.java
+++ src/main/java/com/example/fx_sae/Main.java
@@ -1,8 +1,42 @@
 package com.example.fx_sae;
 
 import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+
+    public void start(Stage stage) throws IOException {
+        Image image = new Image("C:\\Users\\depla\\OneDrive\\Images\\Herbe.Png");
+BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+Background background = new Background(backgroundImage);
+
+        Parent gridPane = new GridPane();
+        for (int row = 20; row < 30; row++) {
+    for (int col = 20; col < 30; col++) {
+        Button button = new Button();
+        button.setPrefSize(50, 50);
+        button.setBackground(background);
+        button.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        ((GridPane) gridPane).add(button, col, row);
+    }
+}
+    Scene scene = new Scene(gridPane, 400, 400);
+    stage = new Stage();
+    stage.setScene(scene);
+    stage.show();
+    }
 
     //x correspond a la hauteur
     //y correspond a la droite et la gauche
@@ -41,10 +75,11 @@ public class Main {
         System.out.println("les voisins sont "+récup.getVoisins(récup.GetCellule(8,1)));
         //System.out.println("les voisins sont"+récup.getVoisins(8,8));
         //System.out.println("les voisins sont"+récup.getVoisins(4,4));
-
+        HelloApplication.launch();
 
 
 
         }
+
     }
 
