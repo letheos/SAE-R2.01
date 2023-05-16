dda8263fd4fb53278d41d974131671ddb06ecdc0
Rafale2000 <loick1morneau@icloud.com>
2023-05-16 09:59:50 +0200

    je te remet ton bordel tout peter force

---
+++ src/main/java/com/example/fx_sae/EventAction.java
@@ -0,0 +1,83 @@
+package com.example.fx_sae;
+/*
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+import java.awt.event.*;
+
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+import javax.swing.text.Position;
+import java.io.IOException;
+import java.util.EventObject;
+
+public class EventAction implements EventHandler {
+    private int action;
+
+    public EventAction(int action){
+        this.action = action;
+    }
+    @Override
+    public void handle(Event event) {
+        Image imageHerbe = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Herbe.png");
+        Image imageMur = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Mur.png");
+        Image imageCactus = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\cactus.jpg");
+        Image imageMargeurite = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Margeurites.jpg");
+        Image imageTerre = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Terre.png");
+        //Image imageTerre2 = new Image("C:\\document\\cour\\src\\Terre.png");
+        Image imageMouton = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Mouton.png");
+        Image imageLoup = new Image("C:\\document\\cour\\sae s2\\sae s201-202\\SAE-R2.01-Theos\\Loup.jpg");
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+        BackgroundImage backgroundimageHerbe = new BackgroundImage(imageHerbe, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundImage backgroundimageMur = new BackgroundImage(imageMur, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundImage backgroundImageCactus = new BackgroundImage(imageCactus, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundImage backgroundimageMargeurite = new BackgroundImage(imageMargeurite, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundImage backgroundImageTerre = new BackgroundImage(imageTerre, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundimageHerbe);
+        Background background2 = new Background(backgroundimageMur);
+        Background background3 = new Background(backgroundImageCactus);
+        Background background4 = new Background(backgroundimageMargeurite);
+        Background background5 = new Background(backgroundImageTerre);
+        Button button = (Button) event.getSource();
+        if (this.action == 0){
+            button.setBackground();
+
+        }
+        else if (this.action == 1) {
+
+
+        }
+
+    }
+}
+*/
