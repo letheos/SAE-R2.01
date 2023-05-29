60744e28b0077cccf8ca0ac8f4420a931760d253
FailLink <129163948+FailLink@users.noreply.github.com>
2023-05-29 23:21:52 +0200

    Add files via upload
    commentaire ajouté

--- EventController.java
+++ EventController.java
@@ -1,9 +1,10 @@
 package com.example.sae_parametre;
 
+//importation des fonctions utiles
 import javafx.beans.binding.DoubleBinding;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.Background;
@@ -15,6 +16,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class EventController implements EventHandler<MouseEvent> {
+    //importation des variable pour modifier la fenêtre
     private Text text;
     private Text mode;
     private Text text1;
@@ -22,9 +24,11 @@ public class EventController implements EventHandler<MouseEvent> {
     private BorderPane borderPane;
     private ChoiceBox<String> choixM;
     private Slider vol;
+    private Label vollabel;
     private MediaPlayer mediaPlayer;
 
-    public EventController(Text t, Text m, Text t1, Text c, BorderPane borderPane, MediaPlayer mediaPlayer, ChoiceBox<String> choixM, Slider v) {
+    //création d'un constructeur
+    public EventController(Text t, Text m, Text t1, Text c, BorderPane borderPane, MediaPlayer mediaPlayer, ChoiceBox<String> choixM, Slider v, Label vollab) {
         this.text = t;
         this.mode = m;
         this.text1 = t1;
@@ -32,43 +36,54 @@ public class EventController implements EventHandler<MouseEvent> {
         this.borderPane = borderPane;
         this.choixM = choixM;
         this.vol = v;
+        this.vollabel = vollab;
         this.mediaPlayer = mediaPlayer;
     }
 
+    //création d'un constructeur null
     public EventController() {
     }
 
     @Override
     public void handle(MouseEvent event) {
+        //vérification si la source de l'événement est un bouton boolean
         if (event.getSource() instanceof ToggleButton) {
             ToggleButton sombre = (ToggleButton) event.getSource();
+            //si le bouton est sur le mode inactif, change le texte du bouton, la couleur du texte, et la couleur de fond
             if (sombre.isSelected()) {
                 sombre.setText("Désactiver");
-                borderPane.backgroundProperty().set(Background.fill(Color.BLACK));
+                borderPane.backgroundProperty().set(Background.fill(Color.valueOf("#534643")));
                 text.setFill(Color.WHITE);
                 mode.setFill(Color.WHITE);
                 text1.setFill(Color.WHITE);
                 changer.setFill(Color.WHITE);
-            } else {
+                vollabel.setTextFill(Color.WHITE);
+            } else { //sinon on rétablie le texte du bouton et les couleurs
                 sombre.setText("Activer");
                 borderPane.backgroundProperty().set(Background.fill(Color.WHITE));
                 text.setFill(Color.BLACK);
                 mode.setFill(Color.BLACK);
                 text1.setFill(Color.BLACK);
                 changer.setFill(Color.BLACK);
+                vollabel.setTextFill(Color.BLACK);
             }
-        } else if (event.getSource().getClass().toString().contains("Button")){
+        } else if (event.getSource().getClass().toString().contains("Button")){//si l'événement vient d'un bouton, arrête la musique
             mediaPlayer.stop();
-        } choixM.setOnAction(events -> {
+        } choixM.setOnAction(events -> {//si l'événement vient de la choise box lorsqu'un choix est donner
+            //la valeur du choix est mis dans cette variable
             String newValue = choixM.getValue();
+            //la valeur est modifier, on rajoute .mp3 au fichier puis on cherche le fichier dans le dossier ressouce
             String newMusicPath = getClass().getResource(newValue + ".mp3").toExternalForm();
+            //on arrête l'ancienne musique puis on lance la nouvelle
             mediaPlayer.stop();
             Media newMedia = new Media(newMusicPath);
             mediaPlayer = new MediaPlayer(newMedia);
             mediaPlayer.play();
 
+            //récupération de la valeur de la barre de volume en temps réel pour régler le volume
             DoubleBinding volumeBinding = vol.valueProperty().divide(100);
             mediaPlayer.volumeProperty().bind(volumeBinding);
+            //lecture en boucle de la musique
             mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         });
     }
