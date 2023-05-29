7b016240caf4968d740e529f02a96babec9fbc77
rafale2000 <loick1morneau@icloud.com>
2023-05-30 00:06:01 +0200

    c'est la merde sans ça

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,13 @@ module com.example.fx_sae {
     requires javafx.fxml;
     requires java.rmi;
     requires java.desktop;
+    requires jdk.jfr;
+    requires javafx.media;
 
 
     opens com.example.fx_sae to javafx.fxml;
     exports com.example.fx_sae;
     exports v;
+    exports m;
+    exports c;
 }
