d8fa0d97e7a12ec1410981f58d41f171eea10152
rafale2000 <loick1morneau@icloud.com>
2023-05-30 00:17:38 +0200

    mise a jour pour le bouton sauvegarder et ouvre une boite de dialogue pour mettre le nom voulue pour le nom du fichier

--- src/main/java/c/EventFonction.java
+++ src/main/java/c/EventFonction.java
@@ -4,6 +4,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import m.Labyrinthe;
@@ -13,49 +14,41 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.Objects;
+import java.util.Optional;
 
 import static java.awt.event.MouseEvent.MOUSE_CLICKED;
 
 public class EventFonction implements EventHandler<Event> {
-    private Stage stage;
+
     //private EvenementsMenu evenementM;
     private javafx.scene.control.TextField tf;
     private Labyrinthe lab;
 
-    public EventFonction(Stage s, TextField t, Labyrinthe lab) {
-        this.stage = s;
-        this.tf = t;
-        //this.evenementM = e;
+    public EventFonction(Labyrinthe lab) {
+
+
+
         this.lab = lab;
     }
 
     @Override
     public void handle(Event event) {
-        System.out.println("une banane");
-        if (event.getSource() instanceof Button) {
-            Button but = (Button) event.getSource();
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Saisir du texte");
+        dialog.setHeaderText("Veuillez saisir du texte :");
+        dialog.setContentText("Texte :");
 
-            if (but.getText().contains("ChargerLabyrinthe")) {
-
-
-                String racineProjet = System.getProperty("user.dir");
-                File debut = new File(racineProjet);
-                FileChooser explorateur = new FileChooser();
-
-                explorateur.setInitialDirectory(debut);
-                FileChooser.ExtensionFilter filtreText = new FileChooser.ExtensionFilter("Text", "*.txt");
-                explorateur.getExtensionFilters().add(filtreText);
-                File dossierSelec = explorateur.showOpenDialog(stage);
-                if (dossierSelec != null) {
-                    tf.setText(dossierSelec.getAbsolutePath());
-                    System.out.println(dossierSelec.getAbsolutePath());
-                    String recette = lab.recup(dossierSelec.getAbsolutePath());
-                    lab.recupToLaby(recette);
-                   System.out.println(lab.toString());
-                }
-
-
-            }
+        Optional<String> result = dialog.showAndWait();
+        if(result.isPresent()){
+            String fichier = result.get();
+            lab.sauvegarde(fichier);
+            System.out.println("sauvegarder");
+        }else {
+            lab.sauvegarde("labyrintheSansNom");
         }
+
+        lab.sauvegarde("bla");
     }
-}
+
+    }
+
